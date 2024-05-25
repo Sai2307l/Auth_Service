0ac62cd9e58f63bf2b5260304041429373928ae1
B. Sai Bhaskar <bhamidipati.2022ug1016@iiitranchi.ac.in>
2024-05-25 22:46:50 +0530

    Added get user by id in repo

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,4 +1,6 @@
+const jwt = require("jsonwebtoken");
 const { UserRepo } = require("../repository/index");
+const { JWT_KEY } = require("../config/serverConfig");
 
 class UserService {
   constructor() {
@@ -12,6 +14,24 @@ class UserService {
     } catch (error) {
       console.log("something went wrong in the Service layer");
       throw { error };
+    }
+  }
+
+  createToken(user) {
+    try {
+      const result = jwt.sign(user, JWT_KEY, { expiresIn: "1d" });
+      return result;
+    } catch (error) {
+      console.log("Something went wrong in token creation");
+    }
+  }
+
+  verifyToken(token) {
+    try {
+      const result = jwt.verify(token, JWT_KEY);
+      return result;
+    } catch (error) {
+      console.log("Something went wrong in token validation", error);
     }
   }
 }
